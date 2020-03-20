5d9568771069e7afb0664113f0d57b907e06fcc3
h272101298 <272101298@qq.com>
2020-03-20 17:26:16 +0800

    1234

--- src/api/qiniu.js
+++ src/api/qiniu.js
@@ -1,3 +1,13 @@
+import Axios from "axios";
+
+function getToken(){
+	Axios.get('https://fa.fengniaotuangou.cn/v4/getToken').then(function(response){
+		console.log(response)
+	})
+	.catch(function(error){
+		console.log(error)
+	})
+}
 const qiniu={
 
 	token:'og7Mlfh-qhsdYS2V5-1s2cmeYEinlnJkHKoLwZmg:88KwI1o_stLYTjnNIpWQltGEMew=:eyJzY29wZSI6InNoYW5nY2hlbmdtdWJhbiIsImRlYWRsaW5lIjoxOTMxODE0ODk4fQ==',
