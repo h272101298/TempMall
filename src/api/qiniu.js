f703dfcf7c4b91751404ef501e361a889edb5a87
h272101298 <272101298@qq.com>
2020-03-21 17:26:24 +0800

    123

--- src/api/qiniu.js
+++ src/api/qiniu.js
@@ -10,12 +10,11 @@ function getToken(){
 }
 const qiniu={
 
-	token:'og7Mlfh-qhsdYS2V5-1s2cmeYEinlnJkHKoLwZmg:88KwI1o_stLYTjnNIpWQltGEMew=:eyJzY29wZSI6InNoYW5nY2hlbmdtdWJhbiIsImRlYWRsaW5lIjoxOTMxODE0ODk4fQ==',
-
+	token:getToken(),
 	upurl:'https://upload-z2.qiniup.com',
 
 
-	showurl:'https://sheng.qdbnm.com/'
+	showurl:'https://tu.fengniaotuangou.cn/'
 	// showurl:'https://qu.qdbnm.com/'
 	// showurl:'http://pam213sin.bkt.clouddn.com/'
 };
